12bfe0b37bf5039005bd67cfae954ca5611390ff
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-09-26 18:16:52 -0500

    added user query

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,8 +1,74 @@
+// React Router
+import { useParams } from 'react-router-dom';
+
+// Apollo
+import { gql, useQuery } from '@apollo/client';
+
+// MUI
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Grid from '@mui/material/Grid';
+
+// Custom Types
+import { IUser } from '../types/customTypes';
+
+const GET_USER_DATA = gql`
+  query ($id: ID!) {
+    user(id: $id) {
+      id
+      name
+      username
+      email
+      address {
+        street
+        suite
+        city
+        zipcode
+      }
+      phone
+      website
+      company {
+        name
+      }
+    }
+  }
+`;
+
+interface UserData {
+  user: IUser;
+}
+
 const User = () => {
+  const params = useParams();
+
+  const { loading, error, data } = useQuery<UserData>(GET_USER_DATA, {
+    variables: { id: params.id },
+  });
+
+  console.log(data);
+
+  console.log('params', params);
+
   return (
-    <div>
-      <h1>User Page</h1>
-    </div>
+    <Container>
+      <Typography variant="h1" sx={{ fontSize: '2rem', textAlign: 'center' }}>
+        User Info
+      </Typography>
+      <List sx={{ margin: 'auto', maxWidth: 500 }}>
+        <ListItem>
+          <Grid container spacing={2}>
+            <Grid item xs={6} sx={{ textAlign: 'right' }}>
+              <Typography>Name:</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography>{data?.user.name}</Typography>
+            </Grid>
+          </Grid>
+        </ListItem>
+      </List>
+    </Container>
   );
 };
 
--- src/types/customTypes.ts
+++ src/types/customTypes.ts
@@ -14,3 +14,21 @@ export interface IUserOverview {
   id: string;
   name: string;
 }
+
+export interface IUser {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+  };
+}
